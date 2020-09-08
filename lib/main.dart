e0faa7b169cebd6883ef6b991b103fda066e6c7e
Abhisek Rout <abhishekrout5@gmail.com>
2020-09-09 01:45:46 +0530

    small change

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: "eBook",
+      title: "eBook ",
       //darkTheme: ThemeData.dark(),
       home: AllFiles(),
       themeMode: ThemeMode.system,
